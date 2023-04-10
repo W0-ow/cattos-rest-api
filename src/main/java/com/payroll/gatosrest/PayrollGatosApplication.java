585f44cbddbead68cc6944d9c61232ebffdec60f
Chris <121321031+W0-ow@users.noreply.github.com>
2023-04-10 21:23:50 +0200

    base del proyecto

---
+++ src/main/java/com/payroll/gatosrest/PayrollGatosApplication.java
@@ -0,0 +1,12 @@
+package com.payroll.gatosrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollGatosApplication
+{
+    public static void main(String... args) {
+        SpringApplication.run(PayrollGatosApplication.class, args);
+    }
+}
