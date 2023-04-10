c96c25bd557ba8169659d05b49e0a068c1763706
Chris <121321031+W0-ow@users.noreply.github.com>
2023-04-10 21:27:06 +0200

    creacion de repository con jpa

---
+++ src/main/java/com/payroll/gatosrest/repository/GatosRepository.java
@@ -0,0 +1,8 @@
+package com.payroll.gatosrest.repository;
+
+import com.payroll.gatosrest.entity.Gato;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GatosRepository extends JpaRepository<Gato, Long>
+{
+}
