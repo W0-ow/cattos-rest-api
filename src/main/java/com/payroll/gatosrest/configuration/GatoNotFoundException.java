225a0224be425bf72e83f3b0a379442a8f50f9d0
Chris <121321031+W0-ow@users.noreply.github.com>
2023-04-12 00:46:14 +0200

    ajustes de mensaje en custom exception

--- src/main/java/com/payroll/gatosrest/configuration/GatoNotFoundException.java
+++ src/main/java/com/payroll/gatosrest/configuration/GatoNotFoundException.java
@@ -2,6 +2,6 @@ package com.payroll.gatosrest.configuration;
 
 public class GatoNotFoundException extends RuntimeException {
     public GatoNotFoundException(Long id) {
-        super("Could not found Catto" + id);
+        super("Could not found Mr Catto " + id);
     }
 }
