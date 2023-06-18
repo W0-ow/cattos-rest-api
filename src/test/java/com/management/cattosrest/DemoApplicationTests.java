2567db03303ea5ff3068c5834779613720a9e3c2
christian <crojasantillan@gmail.com>
2023-06-18 23:24:46 +0200

    rename package name tests

--- src/test/java/com/management/cattosrest/DemoApplicationTests.java
+++ src/test/java/com/management/cattosrest/DemoApplicationTests.java
@@ -1,11 +1,12 @@
-package com.example.gatosrest;
+package com.management.cattosrest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
-
+	DemoApplicationTests() {
+	}
 	@Test
 	void contextLoads() {
 	}
